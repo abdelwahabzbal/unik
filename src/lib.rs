9ed3e3d1b93b4f6e5260b66171d20c2db74e09c0
wahabz <wahabz@yandex.com>
2022-01-07 23:35:46 +0100

    Welcome to unik pkg ;D

---
+++ src/lib.rs
@@ -0,0 +1,158 @@
+//! This crate defines a powerful uniform resource name namespace for UUIDs
+//! (Universally Unique Identifier), which are suited for modern use.
+//!
+//! This lib can be used to create unique and reasonably short
+//! values without requiring extra knowledge.
+//!
+//! A UUID is 128 bits long, and can guarantee
+//! uniqueness across space and time.
+#![doc(html_root_url = "https://docs.rs/unik")]
+#![feature(doc_cfg)]
+
+use core::fmt;
+
+use mac_address::MacAddress;
+
+pub type Node = MacAddress;
+
+#[derive(Debug)]
+pub struct Layout {
+    /// The low field of the Timestamp.
+    pub field_low: u32,
+    /// The mid field of the Timestamp.
+    pub field_mid: u16,
+    /// The high field of the Timestamp multiplexed with the version number.
+    pub field_high_and_version: u16,
+    /// The high field of the ClockSeq multiplexed with the variant.
+    pub clock_seq_high_and_reserved: u8,
+    /// The low field of the ClockSeq.
+    pub clock_seq_low: u8,
+    /// IEEE-802 network address.
+    pub node: Node,
+}
+
+impl Layout {}
+
+#[derive(Default, Debug, PartialEq)]
+pub struct UUID([u8; 16]);
+
+impl UUID {
+    /// UUID namespace for domain name system (DNS).
+    pub const DNS: UUID = UUID([
+        0x6b, 0xa7, 0xb8, 0x10, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30,
+        0xc8,
+    ]);
+
+    /// UUID namespace for ISO object identifiers (OIDs).
+    pub const OID: UUID = UUID([
+        0x6b, 0xa7, 0xb8, 0x12, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30,
+        0xc8,
+    ]);
+
+    /// UUID namespace for uniform resource locators (URLs).
+    pub const URL: UUID = UUID([
+        0x6b, 0xa7, 0xb8, 0x11, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30,
+        0xc8,
+    ]);
+
+    /// UUID namespace for X.500 distinguished names (DNs).
+    pub const X500: UUID = UUID([
+        0x6b, 0xa7, 0xb8, 0x14, 0x9d, 0xad, 0x11, 0xd1, 0x80, 0xb4, 0x00, 0xc0, 0x4f, 0xd4, 0x30,
+        0xc8,
+    ]);
+}
+
+impl fmt::Display for UUID {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            fmt,
+            "{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}{:02}",
+            self.0[0],
+            self.0[1],
+            self.0[2],
+            self.0[3],
+            self.0[4],
+            self.0[5],
+            self.0[6],
+            self.0[7],
+            self.0[8],
+            self.0[9],
+            self.0[10],
+            self.0[11],
+            self.0[12],
+            self.0[13],
+            self.0[14],
+            self.0[15],
+        )
+    }
+}
+
+impl fmt::LowerHex for UUID {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            fmt,
+            "{:02x}{:02x}{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}{:02x}{:02x}{:02x}{:02x}",
+            self.0[0],
+            self.0[1],
+            self.0[2],
+            self.0[3],
+            self.0[4],
+            self.0[5],
+            self.0[6],
+            self.0[7],
+            self.0[8],
+            self.0[9],
+            self.0[10],
+            self.0[11],
+            self.0[12],
+            self.0[13],
+            self.0[14],
+            self.0[15],
+        )
+    }
+}
+
+impl fmt::UpperHex for UUID {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            fmt,
+            "{:02X}{:02X}{:02X}{:02X}-{:02X}{:02X}-{:02X}{:02X}-{:02X}{:02X}-{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}",
+            self.0[0],
+            self.0[1],
+            self.0[2],
+            self.0[3],
+            self.0[4],
+            self.0[5],
+            self.0[6],
+            self.0[7],
+            self.0[8],
+            self.0[9],
+            self.0[10],
+            self.0[11],
+            self.0[12],
+            self.0[13],
+            self.0[14],
+            self.0[15],
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn network_address_default() {
+        let node = Node::default();
+        assert_eq!(node, Node::new([0x0, 0x0, 0x0, 0x0, 0x0, 0x0]));
+    }
+
+    #[test]
+    fn uuid_default() {
+        let uuid = UUID::default();
+        assert_eq!(
+            uuid,
+            UUID([0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0])
+        );
+    }
+}
